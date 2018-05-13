472ddbaa17f68ac922df149281b21949aae6f836
Ben <schinn.ben@gmail.com>
2018-05-13 15:44:58 -0600

    added state

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,13 +2,28 @@ import React from 'react';
 
 
 class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      password: ''
+    };
+    this.handleUsername = this.handleUsername.bind(this);
+    this.handlePassword = this.handlePassword.bind(this);
+  }
+  handleUsername(event) {
+    this.setState({ username: event.target.value })
+  }
+  handlePassword(event) {
+    this.setState({ password: event.target.value })
+  }
   render() {
     return (
       <div>
         <h3>Login</h3>
         <form>
-          <input type="email" placeholder="email"/>
-          <input type="password" placeholder="password"/>
+          <input onChange={this.handleUsername} type="email" placeholder="email"/>
+          <input onChange={this.handlePassword} type="password" placeholder="password"/>
         </form>
       </div>
     )
